348fa17a529c7a891975dea75489c8f658a2bfdd
wala-a <walkhanaizi@gmail.com>
2018-05-13 16:04:30 -0400

    repeats working, just need to work on python to accept more input through USB from PSoC

--- usb_echo/usb_echo.cydsn/main.c
+++ usb_echo/usb_echo.cydsn/main.c
@@ -21,7 +21,7 @@ uint8 inst_byte_out, val_byte_out;
 uint16 length;
 int number, repeats = 0, num_lines, current_line_num;
 uint8 receiving, parsing;
-uint8 lines[512][10];
+uint8 lines[10][512];
 int cmd_eval_val;
 uint8 state = 0;
 #define COMMAND 0
@@ -50,7 +50,7 @@ const char *byte_to_binary(int x)
 //serial receive ISR
 CY_ISR(RX_INT){
     int RxData = UART_ReadRxData();
-    /*debug*/ for (int i=0;i<strlen(byte_to_binary(RxData));i++){LCD_PutChar(byte_to_binary(RxData)[i]);}
+    //*debug*/ for (int i=0;i<strlen(byte_to_binary(RxData));i++){LCD_PutChar(byte_to_binary(RxData)[i]);}
     //if we hear back from the R31JP, that means there is an error
     //here, I am decoding the error code
     uint8 errorMsg[512];
@@ -63,7 +63,6 @@ CY_ISR(RX_INT){
         break;
     case 0b00: sprintf(errorMsg, "Success! data was delivered to the R31JP, %d", RxData);
     }
-    //if (RxData == 0b010) strcpy(errorMsg, "received TWO!");
     uint16 errLength = strlen(errorMsg);
     USB_LoadInEP(1, errorMsg, errLength); 
 }
@@ -101,11 +100,13 @@ int main()
             while(USB_GetEPState(1) != USB_IN_BUFFER_EMPTY);    // Wait until our IN EP is empty
             //if we are receiving the lines to be repeated, save the line
             if (receiving) strcpy(lines[current_line_num], buffer);
+            /*debug*/LCD_PutChar('E');
         } else {
+            /*debug*/LCD_PutChar('R');
             strcpy(buffer, lines[current_line_num]);
         }
         LCD_ClearDisplay();
-        //*debug*/ for (int i=0;i<length-2;i++){LCD_PutChar(buffer[i]);}
+        /*debug*/ for (int i=0;i<length-2;i++){LCD_PutChar(buffer[i]);}
         uint8 line[512] = "";
         //strcpy(line, buffer);
         char* word;
@@ -125,6 +126,7 @@ int main()
                     USB_LoadInEP(1, line, length);*/
                     parsing = 0;
                 }
+                //*debug*/LCD_PutChar(cmd);
                 break;
             case ARG1:{
                 int arg1 = atoi(word);
@@ -132,7 +134,7 @@ int main()
                     state = KEYWORD;
                     cmd_motornum = arg1;
                 } else if (cmd == 'd' || cmd == 'r'){
-                    state = EVAL;
+                    state = (cmd=='d')? EVAL : ARG2;
                     cmd_eval_val = arg1;
                 }
                 else {
@@ -145,7 +147,7 @@ int main()
                 break;}
             case KEYWORD:{
                 state = ARG2;
-                /*debug*/ for (int i=0;i<strlen(word);i++){LCD_PutChar(word[i]);}
+                //*debug*/ for (int i=0;i<strlen(word);i++){LCD_PutChar(word[i]);}
                 if (strcmp(word, "spd") == 0)      cmd_keywrd = 's';
                 else if (strcmp(word, "pos") == 0) cmd_keywrd = 'p';
                 else if (strcmp(word, "stop") == 0) {
@@ -156,7 +158,7 @@ int main()
                 else{
                     state = COMMAND;
                     //sprintf(line, "Parse Error: UNKNOWN KEYWORD: %s", word);
-                    strcpy(line, "Parse Error: UNKNOWN KEYWORD");
+                    sprintf(line, "Parse Error: UNKNOWN KEYWORD: %s", word);
                     /*length = strlen(line);
                     USB_LoadInEP(1, line, length);*/
                     parsing = 0;
@@ -164,12 +166,13 @@ int main()
                 break;}
             case ARG2:{
                 /*int*/ number = abs(atoi(word));
+                /*debug*/ for (int i=0;i<strlen(word);i++){LCD_PutChar(word[i]);}
                 //check if the value is a number and if it is within bounds
                 if (cmd == 'r' && number != 0 && word[0] != '-'){
                     state = EVAL;
                     cmd_val = number;
                 }
-                if (cmd == 'm' && number != 0 && number <= 100 ){/*&& 
+                else if (cmd == 'm' && number != 0 && number <= 100 ){/*&& 
                     ((strlen(word) <= 3 && word[0] != '-') || (strlen(word)<=4 && word[0] == '-'))){*/
                     //append to the line the correct sign
                     state = SND8051;
@@ -178,7 +181,7 @@ int main()
                 } else {
                     /*THINGS ARE ENDING UP HERE WHEN THEY ARE VALID AND SHOULD BE FINE*/
                     state = COMMAND;
-                    strcpy(line, "Parse Error: INVALID VALUE");
+                    sprintf(line, "Parse Error: INVALID VALUE: %d", number);
                     /*length = strlen(line);
                     USB_LoadInEP(1, line, length);*/
                     parsing = 0;
@@ -215,18 +218,21 @@ int main()
                     val_byte_out += (value >> i) & 1;
                 }
 
-                strcpy(line, "Success! Data being sent right over.");
+                sprintf(line, "Success! Data being sent right over. line=%d numlines=%d", current_line_num, num_lines);
                 //length = strlen(line);
                 //Communicate this command byte to the R31JP
                 UART_PutChar(val_byte_out);
                 CyDelay(100);
                 UART_PutChar(inst_byte_out);
                 //in the case of repeats, make the appropriate
-                current_line_num ++;
+                if (repeats) {
+                    current_line_num ++;
+                }//sprintf(line, "linenum= %d, was saved as: %s", current_line_num-1, lines[current_line_num-1]);}
                 if (repeats && current_line_num == num_lines){ 
                     current_line_num = 0;
                     repeats --;
                     receiving = 0;
+                    //*debug*/LCD_PutChar('setup');
                 }
                 //*debug*/ for (int i=0;i<strlen(byte_to_binary(val_byte_out));i++){LCD_PutChar(byte_to_binary(inst_byte_out)[i]);}
                 //update the state of the state machine to accept more commands
@@ -241,9 +247,11 @@ int main()
                     num_lines = cmd_val;
                     current_line_num = 0;
                     receiving = 1;
+                    //*debug*/LCD_PutChar('R');
                 }
                 state = COMMAND;
-                strcpy(line, "Success! This command was evaluated in the PSoC.\n\r");
+                //sprintf(line, "numlines=%d, currentline=%d, receiving=%d",num_lines,current_line_num,receiving);
+                sprintf(line, "Success! This command: %c, was evaluated in the PSoC.\n\r", cmd);
                 break;}
             }
         length = strlen(line);
